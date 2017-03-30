03a09c7904fb49c92179f5f8265dcba30bfd782a
Devaio <robert.camp@outlook.com>
2017-03-30 13:36:50 -0600

    sync

--- models/characters.js
+++ models/characters.js
@@ -1,6 +1,28 @@
 var mongoose        = require('mongoose');
 var moment          = require('moment');
 
+var abilitySubSchema = mongoose.Schema({
+    name: String,
+    class: String,
+    levelReq: {type : Number, default : 0},
+    description: String,
+    dmgMod: {type : Number, default : 0}, // (multiplied by weapon dmg probably)
+    validTargets: Array, // (enemy, player, any, etc)
+    effects: [
+        {
+            category: String,
+            dmg: {type : Number, default : 0}, // not necessarily applicable
+            rounds: Number
+        }
+    ], // (dot, slow, stun, dmg decrease, etc)
+    cost: {
+        pool: String, // (mp, hp)
+        amt: {type : Number, default : 0}
+    },
+    cooldown: {type : Number, default : 0}, // (number of rounds)
+    icon: String // (class names)
+})
+
 var characterSchema = mongoose.Schema({
     
     name:             { type: String, },
@@ -24,7 +46,7 @@ var characterSchema = mongoose.Schema({
     deathDate: { type : String },
     user:      { type : mongoose.Schema.ObjectId, ref : 'User' },
     skillPoints : Number,
-    abilities : {type : Array, default : []}
+    abilities : [abilitySubSchema]
 
 });
 
